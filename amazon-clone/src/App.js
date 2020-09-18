a49afc2c41bd7a25b72f894a0ad6c2cb248d2368
Jonathan Diaz <jonathandiaz0914@gmail.com>
2020-09-17 23:15:57 -0400

    almost done with stripe payments

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -4,9 +4,15 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import "./App.css";
+const promise = loadStripe(
+  "pk_test_51HSUB6JIhe0Xur6uGjzmUCywX32cTAWOD5CZv32otft2CA5oDfM0vzGzDZvjsmoDovnBVO8ICZSuYmlnJcEhBvPb00j7AymDwX"
+);
 
 const App = () => {
   const [{ user }, dispatch] = useStateValue();
@@ -39,12 +45,12 @@ const App = () => {
       <div className='app'>
         <Header />
         <Switch>
+          <Route path='/' exact component={Home} />
           <Route path='/login' exact component={Login} />
           <Route path='/checkout' exact component={Checkout} />
-
-          <Route path='/'>
-            <Home />
-          </Route>
+          <Elements stripe={promise}>
+            <Route path='/payment' exact component={Payment} />
+          </Elements>
         </Switch>
       </div>
     </Router>
